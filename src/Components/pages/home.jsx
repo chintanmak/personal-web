2fd32ea29da539bc9d684d6c0e761c839ee26b54
Chintan Mak <chintanmakwana2011@gmail.com>
2023-04-19 01:58:57 +0530

    8th commit

--- src/Components/pages/home.jsx
+++ src/Components/pages/home.jsx
@@ -18,7 +18,20 @@ import circle4 from '../assets/circle4.png';
 import circle5 from '../assets/circle5.png';
 import circle6 from '../assets/circle6.png';
 
+import education from '../assets/education.png';
+import experience from '../assets/experience.png';
+
+import location from '../assets/location.png';
+import phone from '../assets/phone.png';
+import mail from '../assets/mail.png';
+
+import fb from '../assets/facebook.png';
+import insta from '../assets/instagram.png';
+import github from '../assets/github.png';
+import twitter from '../assets/twitter.png';
+
 import murakamiss from '../assets/murakamiclone.vercel.app.jpeg';
+import teamwayss from '../assets/teamway-chintanmak.vercel.app.jpeg';
 
 export default function Home() {
   return (
@@ -167,10 +180,10 @@ export default function Home() {
 
       <div className='latestwork_section'>
         <div className='work_box'>
-          <img src={murakamiss} />
+          <a href='https://murakami-clone.vercel.app/'><img src={murakamiss} /></a>
         </div>
         <div className='work_box'>
-          <img src={murakamiss} />
+          <a href='https://teamway-chintanmak.vercel.app/'><img src={teamwayss} /></a>
         </div>
       </div>
       <button>LOAD MORE</button>
@@ -181,39 +194,103 @@ export default function Home() {
         <h1>Our Resume</h1>
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
       </div>
+
       <div className='resume_section'>
         
         <div className='education_section'>
+        <div className='box_title'>
+        <img src={education} />
         <h2>Education</h2>
+        </div>
           <div className='education_box'>
             <p>March 2013-2017</p>
             <p><b>Computer Science</b></p>
             <p>International university</p>
+            <br />
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <div className='education_box'>
             <p>March 2013-2017</p>
             <p><b>Computer Science</b></p>
             <p>International university</p>
+            <br />
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
         </div>
 
         <div className='experience_section'>
+        <div className='box_title'>
+        <img src={experience} />
         <h2>Experience</h2>
+        </div>
           <div className='experience_box'>
             <p>March 2013-2017</p>
             <p><b>Computer Science</b></p>
             <p>International university</p>
+            <br />
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <div className='experience_box'>
             <p>March 2013-2017</p>
             <p><b>Computer Science</b></p>
             <p>International university</p>
+            <br />
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
         </div>
+      </div>
+    </div>
+
+    <div className='contactme_container'>
+      <div className='contactme_title'>
+        <h1>Contact me</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+      </div>
+
+      <div className='contactform_section'>
+      <div className='row1'>
+        <div className='formrow1'>
+          <input type='text' placeholder='Full Name'/>
+          <input type='text' placeholder='Email' />
+         <input type='text' placeholder='Subject' />
+        </div>
+        <div className='formrow2'>
+          <textarea type='text' placeholder='Your Message' />
+        </div>
+        <div className='formrow3'>
+          <button>
+            Send me
+          </button>
+        </div>
+      </div>
+      <div className='row2'>
+       <div className='address_section'>
+          <img src={location} />
+          <p>shantinagar, mira road(e), <br />Thane-401107</p>
+       </div>
+       <div className='address_section'>
+          <img src={phone} />
+          <p>+91 9699421928</p>
+       </div>
+       <div className='address_section'>
+          <img src={mail} />
+          <p>chintanmakwana2011@gmail.com</p>
+       </div>
+      </div>
+      </div>
+    </div>
+
+    <div className='footer_container'>
+      <div className='copyright_section'>
+        <p>Copyright © 2023, All rights Reserved,</p>
+        <p>Created by ChintanMak</p>
+      </div>
+
+      <div className='social_links'>
+      <a href='https://www.facebook.com/chintan.makwana.313/'><img src={fb} /></a>
+      <a href='https://instagram.com/chintan.mak'><img src={insta} /></a>
+      <a href='https://github.com/chintanmak'><img src={github} /></a>
+      <a href='https://twitter.com/chintanMac'><img src={twitter} /></a>
       </div>
     </div>
     </>
